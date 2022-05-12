bc85714e6fcc43aafcbda3171e7bb365d55a8364
huulamnguyen <liamnguyen.swe@gmail.com>
2022-05-11 22:12:15 -0400

    completed List Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,11 +12,11 @@ const Header = () => {
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" className={classes.title}>
-          Travel Advisor
+          Travel Buddy
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
-            Explore new places
+            What you wanna go?
           </Typography>
           {/* <Autocomplete> */}
             <div className={classes.search}>

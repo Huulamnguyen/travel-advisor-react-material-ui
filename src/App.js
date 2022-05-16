b27535d70ebfe05d89812b4f7fedacfe111cba16
huulamnguyen <liamnguyen.swe@gmail.com>
2022-05-15 20:11:57 -0400

    working on APIs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CssBaseline, Grid } from '@mui/material';
+
+// import API
+import { getPlacesData } from './api';
 
 // import components
 import Header from './components/Header/Header';
@@ -7,6 +10,26 @@ import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
+
+    const [ places, setPlaces ] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude }}) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        })
+    }, [])
+
+    useEffect(() => {
+        getPlacesData(bounds.sw, bounds.ne)
+        .then((data) => {
+            console.log(data);
+            setPlaces(data);
+        });
+    }, [ coordinates, bounds ]);
+
     return (
         <>
             <CssBaseline/>
@@ -16,7 +39,11 @@ const App = () => {
                     <List/>
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Map/>
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
                 </Grid>
             </Grid>
         </>
